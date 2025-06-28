2ce98c1279f96aed415c4a0a1a91d5eb3e5151fa
shima <koji02140117@gmail.com>
2025-06-29 08:53:38 +0900

    フロント側のコンテナとバック側のコンテナで相互に通信ができることを確認した

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    fetch('/api/')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.Hello))
+      .catch((err) => console.error("Error fetching data:", err))
+  }, [])
 
   return (
     <>
@@ -24,6 +32,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <p>Message from backend: {message}</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
