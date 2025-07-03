9bcd46084fed19df13f711da768c055a189807f6
shima <koji02140117@gmail.com>
2025-07-03 23:25:59 +0900

    DBを拡張した

--- backend/src/database.py
+++ backend/src/database.py
@@ -13,16 +13,18 @@ async def create_tables():
         conn = get_db_connection()
         cur = conn.cursor()
         cur.execute("""
-            CREATE TABLE IF NOT EXISTS items (
+            CREATE TABLE IF NOT EXISTS videos (
                 id SERIAL PRIMARY KEY,
                 url TEXT NOT NULL,
                 title VARCHAR(255) NOT NULL,
-                channel_name VARCHAR(255) NOT NULL
+                channel_name VARCHAR(255) NOT NULL,
+                tags TEXT,
+                memo TEXT
             );
         """)
         conn.commit()
         cur.close()
-        print("Database table 'items' ensured.")
+        print("Database table 'videos' ensured.")
     except Exception as e:
         print(f"Error during database startup: {e}")
     finally:
