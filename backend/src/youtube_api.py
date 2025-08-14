06cea93950a22e54e624bb09a4f77371f6c103be
shima <koji02140117@gmail.com>
2025-08-14 23:44:49 +0900

    環境変数を必要な時のみ使うようにした

--- backend/src/youtube_api.py
+++ backend/src/youtube_api.py
@@ -4,13 +4,12 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from youtube_transcript_api import YouTubeTranscriptApi
 
-YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
-
 def get_youtube_video_details(video_id: str):
-    if not YOUTUBE_API_KEY:
+    youtube_api_key = os.getenv("YOUTUBE_API_KEY")
+    if not youtube_api_key:
         raise ValueError("YouTube API key is not set.")
     
-    youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
+    youtube = build('youtube', 'v3', developerKey=youtube_api_key)
     
     try:
         request = youtube.videos().list(
